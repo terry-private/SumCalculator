0334d414df4c1675b3227a06e31a6c534e225d1a
teruhito <ataeteruhito@gmail.com>
2020-11-08 18:31:53 +0900

    add struct CalcItem CalcItemを作りました。

---
+++ SumCalculator/Model/CalcItem.swift
@@ -0,0 +1,16 @@
+//
+//  CalcItem.swift
+//  SumCalculator
+//
+//  Created by 若江照仁 on 2020/11/08.
+//
+
+import Foundation
+
+struct CalcItem {
+    let id: String = ""
+    var name: String = ""
+    var unitPrice: Int = 0
+    var quantity: Int = 0
+    
+}
