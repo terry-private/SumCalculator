b0f1aee053fb9c07772d37309414bca2d2e74c64
teruhito <ataeteruhito@gmail.com>
2020-11-15 15:43:30 +0900

    Float.currencyのテストを追加しました。 テストして気づいたバグを修正済み ロケールは日本に絞ってます。

--- SumCalculator/Extensions/Float - Extension.swift
+++ SumCalculator/Extensions/Float - Extension.swift
@@ -8,15 +8,27 @@
 import Foundation
 
 extension Float {
+    
+    
+    /// 日本円のみの対応とします。
+    /// ・小数点以下は第二位までとします。
+    /// ・小数点以下の最後の桁が０の場合は消します。
+    
     var currency: String {
         let amountInt = Int(self)
         let afterDot = self - Float(amountInt)
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
-        formatter.locale = .current
+        formatter.locale = Locale(identifier: "ja_JP")
         var resultString: String = formatter.string(from: NSNumber(value: amountInt)) ?? "0"
         if afterDot != 0 {
+            // ・小数点以下は第二位までとします。
+            // "0.14159265..."の場合は前から4文字取得して先頭の0を削除したもの加えて実現しています。
             resultString += String(String(afterDot).prefix(4).dropFirst(1))
+            // ・小数点以下の最後の桁が０の場合は消します。
+            if resultString.hasSuffix("0") {
+                resultString = String(resultString.dropLast(1))
+            }
         }
         return resultString
     }
---
+++ SumCalculatorTests/FloatTests.swift
@@ -0,0 +1,22 @@
+//
+//  FloatTests.swift
+//  SumCalculatorTests
+//
+//  Created by 若江照仁 on 2020/11/15.
+//
+
+import XCTest
+@testable import SumCalculator
+
+class FloatTests: XCTestCase {
+
+    func testCurrency() throws {
+        var testFloat: Float = 2184.1
+        XCTAssertEqual(testFloat.currency, "¥2,184.1")
+        
+        testFloat = 0.1
+        XCTAssertNotEqual(testFloat.currency, "¥0.10")
+        
+    }
+
+}
